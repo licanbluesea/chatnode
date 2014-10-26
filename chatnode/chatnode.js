926950ea24712269af5b05df921a4ca124f5590b
licanming <licanming@kingsoft.com>
2014-10-26 12:56:03 +0800

    update chanode

--- chatnode/chatnode.js
+++ chatnode/chatnode.js
@@ -1,8 +1,19 @@
-var net = require("net");
-var chatServer = net.createServer;
-chatServer.on("connection",function(client){
-	client.write("Hi!/n");
-	client.write("Bye/n");
-	client.end();
+var net = require ("net");
+var chatServer = net.createServer(),clientList = [];
+indent: Standard input:3: Error:Unexpected end of file
+chatServer.on ("connection", function (client) {
+	client.write ("Hi! \n");
+	clientList.push(client);
+	client.name = client.remoteAddress + ":" + client.remotePort
+	client.on("data",function(data){
+		//console.log(data);
+		for(var i=0;i<clientList.length;i++){
+			clientList[i].write(data);
+		}
+	});
+	//client.write("Bye! /n");
+	//client.end();
 });
-chatServer.listen(9000);
+chatServer.listen(9000,function(){
+	console.log("sucess in 9000");
+});
